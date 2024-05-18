849b00fed7b9240a5afa9434b0fbdf48831d47c0
Ramon Hoyo <ramonandreshoyodiaz@gmail.com>
2024-05-17 20:00:58 -0400

    solution

--- controllers/trades.js
+++ controllers/trades.js
@@ -1,1 +1,19 @@
+const Trade = require('../models/trades');
 
+function createTrade(body) {
+  return Trade.create(body);
+}
+
+function findAll() {
+  return Trade.findAll();
+}
+
+function findOne(id) {
+  return Trade.findByPk(id);
+}
+
+module.exports = {
+  createTrade,
+  findAll,
+  findOne,
+};
--- routes/trades.js
+++ routes/trades.js
@@ -1,5 +1,52 @@
 const express = require('express');
+const tradesController = require('../controllers/trades');
 const router = express.Router();
 
+router.post('/', async function(req, res, next) {
+  try {
+    const result = await tradesController.createTrade(req.body);
+    res.status(201).send(result);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
+
+router.get('/', async function(req, res, next) {
+  try {
+    const result = await tradesController.findAll();
+    res.send(result);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
+
+router.get('/:id', async function(req, res, next) {
+  const id = req.params.id;
+  if (!id) {
+    return res.sendStatus(404);
+  }
+
+  try {
+    const result = await tradesController.findOne(id);
+    if (!result) {
+      return res.status(404).send('ID not found');
+    }
+    res.send(result);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
+
+router.patch('/:id', function(req, res) {
+  res.sendStatus(405);
+});
+
+router.delete('/:id', function(req, res) {
+  res.sendStatus(405);
+});
+
+router.put('/:id', function(req, res) {
+  res.sendStatus(405);
+});
 
 module.exports = router;
